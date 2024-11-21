6f98ca025491194d73a22b37636b6f3a36728d67
Stanislav <solarcoaster.st@gmail.com>
2024-11-21 14:47:04 +0200

    updated API

--- src/utils/axios/axios.utils.ts
+++ src/utils/axios/axios.utils.ts
@@ -3,7 +3,7 @@ import { Article } from "../../store/article/article.types";
 import { News } from "../../store/news/news.types";
 
 export const getNews = () =>
-  axios.get<News[]>("https://api.spaceflightnewsapi.net/v3/articles?_limit=50");
+  axios.get<News[]>("https://api.spaceflightnewsapi.net/v4/articles?_limit=50");
 
 export const getArticle = (id: string | number) =>
-  axios.get<Article>(`https://api.spaceflightnewsapi.net/v3/articles/${id}`);
+  axios.get<Article>(`https://api.spaceflightnewsapi.net/v4/articles/${id}`);
